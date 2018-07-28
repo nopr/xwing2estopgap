688d4ccb95b05cdb60554472dae9665d08401d1d
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2018-07-28 10:15:45 +0100

    Initial commit

---
+++ src/app/model/ship.ts
@@ -0,0 +1,35 @@
+export class Ship {
+
+  name: string;
+  cost: number;
+  pilot: string;
+  unique: boolean;
+  faction: string;
+  shipAbility: string;
+  pilotAbility: string;
+  talent: string;
+  system: string;
+  cannon1: string;
+  cannon2: string;
+  torpedo1: string;
+  modification1: string;
+  modification2: string;
+  modification3: string;
+  crew1: string;
+  crew2: string;
+  crew3: string;
+  gunner1: string;
+  astromech: string;
+  force: string;
+  turret: string;
+  title: string;
+  device1: string;
+  device2: string;
+  missile1: string;
+  missile2: string;
+  configuration: string;
+  illicit1: string;
+  illicit2: string;
+  tech: string;
+}
+
---
+++ src/app/model/upgrade.ts
@@ -0,0 +1,9 @@
+export class Upgrade {
+
+  name: string;
+  cost: number;
+  unique: boolean;
+  ability: string;
+  type: string;
+  restriction: string;
+}
