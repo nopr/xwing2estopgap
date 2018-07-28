8fb403881a368303ff6739c4fd61119c3fd38e48
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2018-07-28 13:14:28 +0100

    Adds Bootstrap, jQuery and ng-bootstrap. Adds tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { ShipComponent } from './components/ship/ship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
